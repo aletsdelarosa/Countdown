97f674a6ce55442983c14fb25a15e78868f7319c
Alex de la Rosa <alexdelarosacortes@gmail.com>
2023-07-05 13:50:58 -0600

    Initial Commit

---
+++ Countdown/CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownApp.swift
+//  Countdown
+//
+//  Created by Alex de la Rosa on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
